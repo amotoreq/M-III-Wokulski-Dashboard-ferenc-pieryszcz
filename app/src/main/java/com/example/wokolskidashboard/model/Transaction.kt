f1a5a30df41e57537bb6d2ffd6823a36612b2314
amotoreq <antoni.ferenc@cyfroweszkoly.pl>
2026-04-25 18:56:35 +0200

    Extend Transaction data class

--- app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
+++ app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
@@ -3,5 +3,8 @@ package com.example.wokolskidashboard.model
 data class Transaction(
     val name: String,
     val amount: Double,
-    val isExpense: Boolean
+    val isExpense: Boolean,
+    val category: String? = null,
+    val isOptional: Boolean = false
 )
+
